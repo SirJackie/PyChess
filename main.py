c7ffacec912438b1eee13fdd9ee75fcac81396b8
lyj00912 <3122742760@qq.com>
2023-03-11 18:38:44 +0800

    implemented any x*y chessboard initialization

--- main.py
+++ main.py
@@ -10,24 +10,23 @@ class ChessBoard:
     def Inspect(self):
         print(self.width, self.height, self.info)
 
-    def __init__(self):
-        # self.info = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        self.info = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        self.width = 3
-        self.height = 3
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+        self.info = [[0 for i in range(0, w)] for i in range(0, h)]
 
     def Draw(self):
         for y in range(0, self.height):
-            printf("-------------")
+            printf("-" * (1 + 4 * self.width))
             printf("\n")
             printf("| ")
             for x in range(0, self.width):
                 printf(self.info[y][x])
                 printf(" | ")
             printf("\n")
-        printf("-------------")
+        printf("-" * (1 + 4 * self.width))
 
 
-cb = ChessBoard()
+cb = ChessBoard(5, 5)
 # cb.Inspect()
 cb.Draw()
